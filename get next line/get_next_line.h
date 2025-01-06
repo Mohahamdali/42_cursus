b94536d03bd1c85e69e0b9e7861f2e73fc829056
Mohamed hamdali <152216701+Mohahamdali@users.noreply.github.com>
2025-01-06 18:08:47 +0100

    Add files via upload

---
+++ get next line/get_next_line.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mhamdali <mhamdali@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/06 13:22:58 by mhamdali          #+#    #+#             */
+/*   Updated: 2025/01/06 18:01:53 by mhamdali         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stddef.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1220300030404UL
+# endif
+
+char	*ft_strchr(const char *str, int c);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strdup(const char *src);
+char	*get_next_line(int fd);
+size_t	ft_strlen(const char *str);
+#endif
