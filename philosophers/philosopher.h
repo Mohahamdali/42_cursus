4d12af62d259b1bb29e2e067e1282aa38efc78f2
Mohamed hamdali <152216701+Mohahamdali@users.noreply.github.com>
2025-05-06 06:04:47 +0100

    Add files via upload

---
+++ philosophers/philosopher.h
@@ -0,0 +1,16 @@
+#ifndef PHILOSOPHER_H
+#define PHILOSOPHER_H
+
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <pthread.h>
+
+
+
+int	ft_atoi(const char *str);
+void check_overflow (long n);
+void    check_valid_numbre (char *str);
+#endif
